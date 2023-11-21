cb76465ccbbd866628b99f96910c74915433c967
Biyenga <aimerance.biyenga@kadea.local>
2023-11-21 19:09:57 +0100

    creation de composant header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import start from '../icones/Top-Tweets.png'
+function Header(){
+    return(
+        <div className='header'>
+            <h2 className='page-title'>Home</h2>
+            <a href="#"> <img src={start} alt="image start" className='top-tweets' /></a>
+        </div>
+    )
+}
+
+export default Header;
