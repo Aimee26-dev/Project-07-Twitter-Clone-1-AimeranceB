97d16d617674928af42b17b95f4adc8996c85e31
aimee aimee <Biyenga26@gmail.com>
2024-01-19 19:20:38 +0100

    modification de composant sidebar avec ses classes css

--- src/components/TweetEditorForm.jsx
+++ src/components/TweetEditorForm.jsx
@@ -4,7 +4,7 @@ import TweetEditorButton from './TweetEditorButton'
 function TweetEditorForm(){
     return(
         <section className='tweet-editor'>
-            <aside className='avatar'> <a href="#"><img src={profile} alt="image profile" /></a></aside>
+            <aside className='avatar'> <img src={profile} alt="image profile" /></aside>
             <div className='tweet-editor-form'> 
             <input type="text" placeholder="What's happening" className='tweet-editor-input' />
           
--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -15,7 +15,70 @@ import iconeprivate from '../icones/iconeprivate.png';
 function Sidebar({props}){
   return(
     <div className="sidebar">
-      
+           <ul className="sidebar-navbar">
+          <li>
+      <NavLink to="/Iconetweet" className="sidebar-navbar-li">
+            <img src={iconetweet} alt="image" />
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/" className="sidebar-navbar-li">
+           <img src={imagehometweet} alt="image" />Home
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/Explore" className="sidebar-navbar-li">
+           <img src={iconeexploretweet} alt="image" />Explore
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/Notifications" className="sidebar-navbar-li">
+           <img src={iconenotificationtweet} alt="icone" />Notifications
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/Messages" className="sidebar-navbar-li">
+            <img src={iconemessage} alt="icone" />Messages
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/Bookmark" className="sidebar-navbar-li">
+           <img src={iconebook} alt="icone" />Bookmarks
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/Iconelists" className="sidebar-navbar-li">
+           <img src={iconelists} alt="icone" />Lists
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/Profil" className="sidebar-navbar-li">
+            <img src={iconeprofile} alt="icone" />Profil
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/More" className="sidebar-navbar-li">
+           <img src={iconemore} alt="icone" />More
+          </NavLink>
+          </li>
+          <div className="sidebar-button"> 
+          <Button/>
+          </div>
+          </ul>
+          
+          <div className="avatar-div">
+            <NavLink to="/Profil"><img src={profilephoto} alt="imageprofil" /></NavLink>
+              <div className="ortizdiv">
+                  <div>
+                  <h3>Bradley Ortiz</h3>
+                  <span>
+                      <img src={iconeprivate} alt="icone private" />
+                  </span>
+                  </div>
+              <span className="namefollow-span" id="@bradley">@bradley ..</span>
+              </div>
+          </div>
+          <Outlet/> 
     </div>
   )
 }
